510e1be287f56ae540a2605a1e2ad2f80a5f9a5a
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2023-06-12 17:51:22 -0700

    add

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    new_list = list(my_list)
+    new_list[idx] = element
+    return new_list
