915c81d147c4f82e9e488e58e9e76c1e40707a4a
Olabanji Enioluwayan <olabanjienioluwayan@gmail.com>
2023-06-27 15:17:21 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,11 +3,9 @@ class square:
     def __init__(self, size=0):
 
         if not isinstance(size, int):
-            raise TypeError:
-                print("size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError:
-                print("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self._size = size
 
     def area(self):
