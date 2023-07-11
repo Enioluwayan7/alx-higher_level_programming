2cd922baeae29335dfd563084cdde4d0420a0fb7
Olabanji Enioluwayan <olabanjienioluwayan@gmail.com>
2023-07-11 14:25:20 -0700

    3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """Defines a file-writing function."""
 import json
 
+
 def to_json_string(my_obj):
-    """function that returns the JSON representation of an object (string)"""
+    """function that returns the JSON representation of an object (string)."""
     return json.dumps(my_obj)
