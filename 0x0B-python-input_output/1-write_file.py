67ee9bbb15ea644b3d84f06f514c86a6e66384bd
Olabanji Enioluwayan <olabanjienioluwayan@gmail.com>
2023-07-11 13:06:58 -0700

    addd

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Defines a text reading function"""
+
 def write_file(filename="", text=""):
+
+    """ writes a string to a text file (UTF8)
+    Args:
+        filename (str): the name of file to be written on.
+        text (str): The text to write a file.
+    Return:
+        Number of character written"""
+
     with open('filename', 'w', encoding="utf-8") as f:
         return f.write(text)
