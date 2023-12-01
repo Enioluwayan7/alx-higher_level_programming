8bb6461b048ff1b1932b1a63b09b117a9df2e6b4
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2023-11-30 16:22:06 -0800

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,14 +2,13 @@
 
 # Check if a URL is provided as an argument
 if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
+	echo "Usage: $0 <URL>"
+	exit 1
 fi
-
 url="$1"
 
 # Use curl to send a request and retrieve the body size
-body_size=$(curl -sI "$url" | grep -i "content-length" | awk '{print $2}' | tr -d '\r\n')
+body_size = $(curl -sI "$url" | grep -i "content-length" | awk '{print $2}' | tr -d '\r\n')
 
 # Check if the Content-Length header is present in the response
 if [ -z "$body_size" ]; then
