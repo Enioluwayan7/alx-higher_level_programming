cd33ec1adee04df52a4916a3d63c0e9d56a4dd14
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2024-07-31 17:56:21 -0700

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Get the file path and string to write from the arguments
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+// Write the string to the file in UTF-8 encoding
+fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`String written to file ${filePath}`);
+});
