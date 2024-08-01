4a196bed3f56c49f4d234739da396c6387077761
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2024-07-31 17:53:19 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,20 +1,15 @@
 #!/usr/bin/node
-/*
- script that reads and prints the content of a file.
-*/
-import sys
 
-def read_file(file_path):
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content)
-    except Exception as e:
-        print(f"Error occurred: {e}")
+const fs = require('fs');
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <file_path>")
-    else:
-        file_path = sys.argv[1]
-        read_file(file_path)
+// Get the file path from the first argument
+const filePath = process.argv[2];
+
+// Read the file in UTF-8 encoding
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
